cb2d0877d8021848d6b51c45eb6b53516f958e25
alex-voltagency <80480705+alex-voltagency@users.noreply.github.com>
2021-10-01 15:21:17 -0400

    add input button

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,12 +17,20 @@ module.exports = {
         '10px' : '0.625rem',
         '100px': '6.25rem',
        },
+       fontSize:{
+        
+       },
        padding: {
         '127px': '7.938rem',
         '25px': '1.563rem',
         '41px': '2.563rem',
         '33px': '2.063rem',
         '108px': '6.75rem',
+       },
+       colors: {
+        'color1': '#1E2D69',
+        'color2': '#0C122C',
+        'color3': '#DBD9FF',
        },
     },
   },
