336bad843497048d0cdb33e3bc2718f91ed15249
alex-voltagency <80480705+alex-voltagency@users.noreply.github.com>
2021-10-01 22:29:33 -0400

    add bottom section

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,17 @@ module.exports = {
         '100px': '6.25rem',
        },
        fontSize:{
+        '10px': '2.5rem',
         
+       },
+       lineHeight: {
+        'lh14px': '0.875rem',
+       },
+       letterSpacing:{
+        '1px': '0.063rem',
+       },
+       translate: {
+
        },
        padding: {
         '127px': '7.938rem',
@@ -26,6 +36,15 @@ module.exports = {
         '41px': '2.563rem',
         '33px': '2.063rem',
         '108px': '6.75rem',
+        '3px': '0.188rem',
+        '52px': '3.25rem',
+        '74px': '4.625rem',
+        '9px': '0.563rem',
+        '10px': '0.625rem',
+
+       },
+       margin:{
+        '73px': '4.563rem',
        },
        colors: {
         'color1': '#1E2D69',
